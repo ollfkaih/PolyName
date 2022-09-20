fed7532702402095d8f96a9166be90f5742f6d6d
fredrfha <fredrfha@stud.ntnu.no>
2022-09-20 18:43:16 +0200

    Eva og forside/adam

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaView, View, StyleSheet, Button } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import React from 'react';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -13,10 +16,37 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <SafeAreaView style={styles.container}>
+            <View>
+              <Text style={styles.heading}>PolyName</Text>
+              <Text>leaderboard</Text>
+            </View>
+          </SafeAreaView>
+          <SafeAreaView style={styles.startArea}>
+            <View>
+              <Button title={"Start Quiz"}>Start quiz</Button>
+            </View>
+          </SafeAreaView>
+        </ApplicationProvider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.8,
+    backgroundColor: '#fcabfc',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 30,
+  },
+  startArea: {
+    flex: 0.2,
+    backgroundColor: '#111',
+    fontSize: 50,
+    height: 100
+  }
+});
---
+++ react-native/screens/MainPageScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function MainPageScreen() {
+    return (
+        <View>
+            <Text></Text>
+            
+        </View>
+    );
+}
+
+export default MainPageScreen;
