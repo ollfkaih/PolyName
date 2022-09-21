8b7febfc43cf5b8e3bb7cc9f13202db420553459
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-09-21 08:37:51 +0200

    add progress bar component

---
+++ react-native/components/QuizProgressBar.tsx
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+import { Layout } from '@ui-kitten/components';
+import { AnswerStatus, Quiz } from '../types';
+
+interface Props {
+  quiz: Quiz;
+  answers: AnswerStatus[];
+  currentQuestionIndex: number;
+}
+
+export const QuizProgressBar = ({ quiz, answers, currentQuestionIndex }: Props) => {
+  return (
+    <Layout style={styles.container}>
+      {quiz.questions.map((question, index) => {
+        let color: string;
+        switch (answers[index]) {
+          case 'CORRECT':
+            color = 'green';
+            break;
+          case 'INCORRECT':
+            color = 'red';
+            break;
+          case 'UNANSWERED':
+            color = 'gray';
+            break;
+        }
+
+        return (
+          <Layout
+            key={index}
+            style={{
+              ...styles.element,
+              backgroundColor: index === currentQuestionIndex ? 'blue' : color,
+            }}
+          />
+        );
+      })}
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  element: {
+    flex: 1,
+    height: 6,
+    width: 10,
+    marginLeft: 4,
+    marginRight: 4,
+    borderRadius: 10,
+  },
+});
